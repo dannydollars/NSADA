b0a982d634ecb3f71666652429c4a8900bfa91a7
dannydollars <danielcmatheson@gmail.com>
2024-03-11 15:22:03 -0300

    Update index.js

--- Catch/index.js
+++ Catch/index.js
@@ -1,11 +1,6 @@
 module.exports = async function (context, req) {
  
- async function parsereq(req) {
-context.log(req)
-
- }
-
- parsereq(req);
+context.log(req.body)
   // Your code logic goes here
  
      context.res = {
